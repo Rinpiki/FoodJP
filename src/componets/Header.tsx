46657fe8c47455facc07a263eb8d8551e79e9fe7
Marcos-Pablo <rinpikibr@Gmail.com>
2023-04-13 19:17:25 -0300

    fix: bug flash corrigido

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -5,12 +5,12 @@ import menu from '../../public/icon/menu.svg'
 function Header() {
   return (
     <header className="flex justify-between p-6 w-full items-center md:p-8">
-      <Fade direction="down">
+      <Fade direction="down" delay={200}>
         <h1 className="font-Montserrat text-cinza font-bold text-lg sm:text-[32px]">
           Food<span className="text-red">JP</span>
         </h1>
       </Fade>
-      <Fade direction="up">
+      <Fade direction="up" delay={200}>
         <ul className="space-x-16 text-xl font-Inter text-cinza font-medium hidden sm:flex md:space-x-32">
           <li className=" hover:text-red">
             <a href="id">inicio</a>
@@ -24,7 +24,7 @@ function Header() {
         </ul>
       </Fade>
 
-      <Fade direction="right">
+      <Fade direction="right" delay={200}>
         <button className="hidden text-xl font-Inter text-cinza font-medium px-9 py-2 rounded-md border-black border sm:flex ">
           Entrar
         </button>
